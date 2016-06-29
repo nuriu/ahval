dfaf9f05a00fcc4b4be460f7da20f9efe15af8e7
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-06-29 16:51:31 +0300

    Ayı ve günü gösteren ufak takvim alanı eklendi.

---
+++ app/src/zaman.ts
@@ -0,0 +1,43 @@
+$(document).ready(function () {
+
+  // saniye
+  setInterval(function () {
+    let saniye = new Date().getSeconds();
+    $("#saniye").html((saniye < 10 ? "0" : "") + saniye);
+  }, 1000);
+
+  // dakika
+  setInterval(function () {
+    let dakika = new Date().getMinutes();
+    $("#dakika").html((dakika < 10 ? "0" : "") + dakika);
+  }, 1000);
+
+  // saat
+  setInterval(function () {
+    let saat = new Date().getHours();
+    $("#saat").html((saat < 10 ? "0" : "") + saat);
+  }, 1000);
+
+  // gün
+  document.getElementById("gun").innerHTML = new Date().getDate().toString();
+
+  // ay
+  let ay = ["Ocak", "Şubat", "Mart", "Nisan", "Mayıs", "Haziran", "Temmuz", "Ağustos", "Eylül", "Ekim", "Kasım", "Aralık"][new Date().getMonth()];
+  document.getElementById("ay").innerHTML = ay;
+
+  // yıl
+  document.getElementById("yil").innerHTML = new Date().getUTCFullYear().toString();
+
+});
+
+
+$(document).ready(function () {
+  $('#gun').html(function () {
+    var date = new Date();
+    return (date.getDate());
+  });
+  $('span.month').html(function () {
+    var date = new Date();
+    return month;
+  });
+});
