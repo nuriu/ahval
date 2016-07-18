c7529c133e9232e535b356d10b40e8f8e89e2ec4
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-07-18 22:23:55 +0300

    Veritabanı düzenlendi, temel etiket sistemi geliştirildi.

--- app/src/liste.ts
+++ app/src/liste.ts
@@ -1,7 +1,7 @@
 /// <reference path="../../typings/index.d.ts" />
 
 $(function () {
-    $("#sortable").sortable({
-        placeholder: "ui-sortable-placeholder"
+    $("#siralanabilir").sortable({
+        placeholder: "siralanabilir-yeni-yer-metni"
     });
 });
--- app/src/notlar.ts
+++ app/src/notlar.ts
@@ -7,5 +7,36 @@ var fb = fs.readFileSync(`${__dirname}/db/veritabani`);
 var db = new SQL.Database(fb);
 
 
-var tn = db.exec("SELECT * FROM NOTLAR");
-console.log(tn[0].values[0]);
+var tariheGoreTumNotlar = db.exec("SELECT * FROM NOTLAR ORDER BY TARIH ASC");
+console.log(tariheGoreTumNotlar[0].values);
+
+$(function () {
+    notlariListele();
+});
+
+function notlariListele() {
+    for (let indis = 0; indis < tariheGoreTumNotlar[0].values.length; indis++) {
+        let eleman = tariheGoreTumNotlar[0].values[indis];
+        let ifade: String = "<li>";
+
+        // TAMAMLANMA DURUMU
+        if (eleman[4] == 1)
+            ifade += "<input id='" + eleman[0] + "' type='checkbox' checked />";
+        else
+            ifade += "<input id='" + eleman[0] + "' type='checkbox' />";        
+
+        // ETİKET DURUMU
+        if (eleman[3] != 0) {
+            let etiketinRengi: String = db.exec("SELECT RENK FROM ETIKETLER WHERE ID="+ eleman[3])[0].values[0];            
+            ifade += "<label for='" + eleman[0] + "' style='border-right: 8px solid " + etiketinRengi + ";'>";
+        } else {
+            ifade += "<label for='" + eleman[0] + "'>";
+        }
+
+        ifade +="\
+        <h2>" + eleman[1] + "<i class='resize vertical icon' style='float:right;'></i><span>" + eleman[2] + "</span></h2>\
+        </label></li>";
+
+        document.getElementById("siralanabilir").innerHTML += ifade;
+    }
+}
