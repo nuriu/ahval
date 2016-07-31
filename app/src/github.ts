e281e5384f3799a1ea6cceae8699feba2d53eda0
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-07-31 15:47:34 +0300

    Proje listesi alanı düzenlendi.

--- app/src/github.ts
+++ app/src/github.ts
@@ -1,6 +1,11 @@
 /// <reference path="../../typings/index.d.ts" />
 
 var KULLANICI: any;
+var PROJELER: any;
+
+$(document).ready(function () {
+    document.getElementById("projeListesi").innerHTML = "<li class='active'><a href='#'><i class='browser icon'></i> Tüm Notlar</a></li>";
+});
 
 $("#githubKullaniciAdi").keyup(function (e) {
     if (e.keyCode == 13) {
@@ -14,12 +19,16 @@ function gitHubBilgileriniGetir() {
     if (kullaniciAdi.length) {
         $.get("https://api.github.com/users/" + kullaniciAdi, function (veri) {
             KULLANICI = veri;
-            gitHubBilgileriniYazdir();
-        }); 
+            $.get(KULLANICI.repos_url, function (veri) {
+                PROJELER = veri;
+                gitHubProjeListesiniYazdir();
+            });
+            gitHubProfilBilgileriniYazdir();
+        });
     }
 }
 
-function gitHubBilgileriniYazdir() {
+function gitHubProfilBilgileriniYazdir() {
     if (KULLANICI) {
         document.getElementById("github").innerHTML = "<div class='ui list'><div class='item'>\
         <img class='ui tiny avatar image' src='"+ KULLANICI.avatar_url +"'>\
@@ -37,5 +46,22 @@ function gitHubBilgileriniYazdir() {
         <div class='statistic'><div class='value'>" + KULLANICI.following + "</div>\
         <div class='label'>TAKİP EDİLEN</div></div>\
         </div>";
+    }
+}
+function gitHubProjeListesiniYazdir() {
+    if (PROJELER) {
+        //console.log(PROJELER);
+        document.getElementById("projeListesiBaslik").innerHTML = "<i class='tasks icon'></i> Projeler (" + PROJELER.length + ")";
+        for (var i = 0; i < PROJELER.length; i++) {
+            let proje = PROJELER[i];
+            $.get("https://api.github.com/repos/nuriu/" + proje.name + "/issues", function (veri) {
+                if (veri != "") {
+                    //console.log(veri);
+                    document.getElementById("projeListesi").innerHTML += "<li><a href='#'>" + proje.name + " (" + veri.length + ")</a></li>";
+                } else {
+                    document.getElementById("projeListesi").innerHTML += "<li><a href='#'>" + proje.name + "</a></li>";
+                }
+            });
+        }
     }
 }
