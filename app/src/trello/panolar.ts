c95b33f3556bdfb77a1204932e1180f74fcad360
Nuri Uzunoğlu <nuriu@users.noreply.github.com>
2016-11-30 13:08:19 +0300

    Organizasyon panolarının baz gösterimi eklendi.
    resolves #25

--- app/src/trello/panolar.ts
+++ app/src/trello/panolar.ts
@@ -1,17 +1,36 @@
 $(document).ready(() => {
+    Trello.get("members/me/organizations", (organizasyonlar) => {
+        organizasyonlar.forEach(organizasyon => {
+            console.log(organizasyon);
+            organizasyonAlanlariniOlustur(organizasyon);
+        });
+    });
     Trello.get("members/me/boards", (panolar) => {
         panolar.forEach(pano => {
             console.log(pano);
             if (pano.starred) {
-                yildizliPanoEkle(pano);
-            } else if (!pano.idOrganization) {
-                kisiselPanoEkle(pano);
+                yildizliPanoEkle(panoAlaniniOlustur(pano));
+            }
+            if (!pano.idOrganization) {
+                kisiselPanoEkle(panoAlaniniOlustur(pano));
+            }
+            if (pano.idOrganization) {
+                organizasyonPanosuEkle(pano.idOrganization, panoAlaniniOlustur(pano));
             }
         });
     });
 });
 
-function yildizliPanoEkle(pano: any) {
+function organizasyonAlanlariniOlustur(organizasyon: any) {
+    let kod: string;
+
+    kod = '<h3><i class="teal users icon"></i> <small>' + organizasyon.displayName + "</small></h3>";
+    kod += '<div class="ui three column grid" id="' + organizasyon.id + '"></div>';
+
+    $("#panolar").append(kod);
+}
+
+function panoAlaniniOlustur(pano: any): string {
     let kod: string;
 
     kod = '<div class="column"><div class="ui fluid card">';
@@ -22,19 +41,17 @@ function yildizliPanoEkle(pano: any) {
     kod += '<div class="ui bottom attached teal button" id="' + pano.id + '">Panoya Geç</div>';
     kod += "</div></div>";
 
+    return kod;
+}
+
+function yildizliPanoEkle(kod: string) {
     $("#yildizliPanolar").append(kod);
 }
 
-function kisiselPanoEkle(pano: any) {
-    let kod: string;
-
-    kod = '<div class="column"><div class="ui fluid card">';
-    kod += '<div class="content">';
-    kod += '<div class="header">' + pano.name + "</div>";
-    kod += '<div class="description">' + pano.desc + "</div>";
-    kod += "</div>";
-    kod += '<div class="ui bottom attached teal button" id="' + pano.id + '">Panoya Geç</div>';
-    kod += "</div></div>";
-
+function kisiselPanoEkle(kod: string) {
     $("#kisiselPanolar").append(kod);
+}
+
+function organizasyonPanosuEkle(organizasyonKimligi: string, kod: string) {
+    $("#" + organizasyonKimligi).append(kod);
 }
