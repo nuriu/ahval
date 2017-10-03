2de3874c92f18e1dfa21656cbee276bfe957c6f0
Nuri Uzunoğlu <dev@nuriuzunoglu.com>
2017-10-03 12:27:37 +0300

    İş sayfasına üst rota bölümü eklendi. (#49)

--- front-end/src/app/github/components/issue/issue.component.ts
+++ front-end/src/app/github/components/issue/issue.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
-
+import { UserService } from '../../../services/user.service';
+import { GitHubService } from '../../services/github.service';
 
 @Component({
     selector: 'app-github-issue',
@@ -8,7 +10,33 @@ import { Component, Input, OnInit } from '@angular/core';
     styleUrls: ['issue.component.css']
 })
 export class IssueComponent implements OnInit {
-    constructor() { }
+    @Input() repoName;
+    @Input() issue;
 
-    ngOnInit() { }
+    constructor(private githubService: GitHubService,
+                private userService: UserService,
+                private route: ActivatedRoute) { }
+
+    ngOnInit() {
+        this.route.params.subscribe(params => {
+            this.userService.getToken('GITHUB').subscribe((res) => {
+                if (res.token) {
+                    this.githubService.setToken(res.token);
+                    this.githubService.activate();
+
+                    if (params.owner != null && params.name != null && params.number != null) {
+                        this.repoName = params.name;
+
+                        this.githubService.getIssueInfo(params.owner, params.name, params.number)
+                        .subscribe((data) => {
+                            this.issue = data;
+                            console.log(this.issue);
+                        });
+                    }
+                } else {
+                    console.log('Error: Could not get github access token of user.');
+                }
+            });
+        });
+    }
 }
