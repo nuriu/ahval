b8087f03c76895237200377616ab6fd4a70c26bb
Nuri Uzunoğlu <dev@nuriuzunoglu.com>
2017-10-03 11:59:44 +0300

    İş sayfası için bağlantı rotası eklendi. (#49)

--- front-end/src/app/github/github-routing.module.ts
+++ front-end/src/app/github/github-routing.module.ts
@@ -4,11 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { GitHubComponent } from './components/github/github.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RepositoryComponent } from './components/repository/repository.component';
+import { IssueComponent } from './components/issue/issue.component';
 
 const githubRoutes: Routes = [
     { path: 'home', component: GitHubComponent },
     { path: 'profile/:login', component: ProfileComponent },
-    { path: 'repository/:owner/:name', component: RepositoryComponent }
+    { path: 'repository/:owner/:name', component: RepositoryComponent },
+    { path: 'issue/:owner/:name/:number', component: IssueComponent }
 ];
 
 @NgModule({
