076d8eb27ba235bb1a2137a4aafde8ac38f2ebcf
Nuri Uzunoğlu <dev@nuriuzunoglu.com>
2017-10-18 13:44:18 +0300

    Not eklemekte kullanılacak viewmodel eklendi. (#53)

---
+++ back-end/ViewModels/AddNoteViewModel.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Ajanda.ViewModels
+{
+    public class AddNoteViewModel
+    {
+        public string Body { get; set; }
+        public string Date { get; set; }
+    }
+}
