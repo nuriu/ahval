cc0f7bcbb8a2809039ed5a8f47aa3f2e24127892
Nuri Uzunoğlu <dev@nuriuzunoglu.com>
2017-10-18 14:13:40 +0300

    Not ekleme işlevi için servis fonksiyonları eklendi. (#53)

--- front-end/src/app/weekly/services/weekly.service.ts
+++ front-end/src/app/weekly/services/weekly.service.ts
@@ -18,6 +18,41 @@ export class WeeklyService {
         }
     }
 
+    addNote(body: string, date: Date) {
+        const data = {
+            'Body': body,
+            'Date': this.processDate(date)
+        };
+
+        return this.http.post(this.APIUrl + '/api/notes/addnote', data, {
+            headers: this.h
+        }).map(res => res.json());
+    }
+
+    getUserNotes() {
+        return this.http.get(this.APIUrl + '/api/notes/getmynotes', {
+            headers: this.h
+        }).map(res => res.json());
+    }
+
+    getUserNotesForDate(date: Date) {
+        const s: URLSearchParams = new URLSearchParams();
+        s.set('date', this.processDate(date));
+        console.log(s);
+        return this.http.get(this.APIUrl + '/api/notes/getnotesfordate', {
+            headers: this.h,
+            search : s
+        }).map(res => res.json());
+    }
+
+    private processDate(date: Date): string {
+        const day = date.getDate();
+        const month = date.getMonth() + 1;
+        const year = date.getFullYear();
+
+        return day + '.' + month + '.' + year;
+    }
+
     private setAuthorizationHeader() {
         this.h.append('Authorization', 'bearer ' + localStorage.getItem('ajanda_auth_token'));
     }
