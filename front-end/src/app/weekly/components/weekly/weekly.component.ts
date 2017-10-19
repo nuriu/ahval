a244ec428ae0d960a390d09594fb2a9bc75e3827
Nuri Uzunoğlu <dev@nuriuzunoglu.com>
2017-10-19 11:31:03 +0300

    Haftalık sayfasındaki veri yapısı güncellendi. (#53)
    Alınan notlar ilgili günde listelenmesi sağlandı.

--- front-end/src/app/weekly/components/weekly/weekly.component.ts
+++ front-end/src/app/weekly/components/weekly/weekly.component.ts
@@ -3,7 +3,10 @@ import { Component, Input, OnInit } from '@angular/core';
 import { UserService } from '../../../services/user.service';
 import { WeeklyService } from '../../services/weekly.service';
 
-
+interface DateItemViewModel {
+    date: Date;
+    items: Array<Object>;
+}
 
 @Component({
     selector   : 'app-weekly',
@@ -11,37 +14,63 @@ import { WeeklyService } from '../../services/weekly.service';
     styleUrls  : ['./weekly.component.css']
 })
 export class WeeklyComponent implements OnInit {
-    @Input() dates: Array<Date>;
+    @Input() itemsPerDate: Array<DateItemViewModel>;
 
     constructor(private weeklyService: WeeklyService,
                 private userService: UserService) {}
 
     ngOnInit() {
         this.weeklyService.activate();
-        this.dates = new Array<Date>();
+
         this.fillDates(new Date());
+        this.fillNotes();
     }
 
     fillDates(date: Date) {
+        this.itemsPerDate = new Array<DateItemViewModel>();
         const monday = new Date();
         monday.setDate(date.getDate() - date.getDay() + (date.getDay() === 0 ? -6 : 1));
 
-        this.dates.push(monday);
+        this.itemsPerDate.push({
+            date : monday,
+            items: new Array<Object>()
+        });
 
         for (let i = 0; i < 6; i++) {
-            this.dates.push(new Date(date.setDate(monday.getDate() + i + 1)));
+            this.itemsPerDate.push({
+                date : new Date(date.setDate(monday.getDate() + i + 1)),
+                items: new Array<Object>()
+            });
         }
+    }
+
+    fillNotes() {
+        this.weeklyService.getUserNotesForWeek(this.itemsPerDate[0].date).subscribe(res => {
+            res.forEach(note => {
+                this.itemsPerDate.forEach(element => {
+                    if (note.date === this.weeklyService.processDate(element.date)) {
+                        element.items.push(note);
+                    }
+                });
+            });
+        });
     }
 
     incrementWeek() {
-        for (let i = 0; i < this.dates.length; i++) {
-            this.dates[i].setDate(this.dates[i].getDate() + 7);
+        for (let i = 0; i < this.itemsPerDate.length; i++) {
+            this.itemsPerDate[i].date.setDate(this.itemsPerDate[i].date.getDate() + 7);
         }
+
+        this.fillDates(this.itemsPerDate[0].date);
+        this.fillNotes();
     }
 
     decrementWeek() {
-        for (let i = 0; i < this.dates.length; i++) {
-            this.dates[i].setDate(this.dates[i].getDate() - 7);
+        for (let i = 0; i < this.itemsPerDate.length; i++) {
+            this.itemsPerDate[i].date.setDate(this.itemsPerDate[i].date.getDate() - 7);
         }
+
+        this.fillDates(this.itemsPerDate[0].date);
+        this.fillNotes();
     }
 }
--- front-end/src/app/weekly/services/weekly.service.ts
+++ front-end/src/app/weekly/services/weekly.service.ts
@@ -45,7 +45,7 @@ export class WeeklyService {
         }).map(res => res.json());
     }
 
-    private processDate(date: Date): string {
+    processDate(date: Date): string {
         const day = date.getDate();
         const month = date.getMonth() + 1;
         const year = date.getFullYear();
